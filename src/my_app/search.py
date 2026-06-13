16f28e55bcdcfdaab46211bc7c645e2cc8022213
Albin Berge <albin@prologic.se>
2026-06-13 17:44:15 +0200

    test case

---
+++ src/my_app/search.py
@@ -0,0 +1,3 @@
+def add(x,y):
+    """" lägger ihop två tal och returnerar resultatet """
+    return x + y
---
+++ tests/test_search.py
@@ -0,0 +1,12 @@
+from my_app.search import add
+def test_add():
+    #Arrange
+    expected = 3
+    x = 1
+    y = 2
+
+    # Act
+    actual = add(x,y)
+
+    #Assert
+    assert actual == expected
